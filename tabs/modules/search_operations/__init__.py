f607219b5cfefed6f4c911f9370559f8373906ac
futurefashionhub <119069472+future-fashion-hub@users.noreply.github.com>
2025-12-20 14:20:43 +0300

    Add WindowFunctionDialog to exports

--- tabs/modules/search_operations/__init__.py
+++ tabs/modules/search_operations/__init__.py
@@ -1,25 +1,3 @@
-"""
-Диалоги для операций поиска и выборки данных
-"""
+from .window_functions_dialog import WindowFunctionDialog
 
-from .text_search_dialog import TextSearchDialog
-from .advanced_select_dialog import AdvancedSelectDialog, AggregateFunctionDialog, GroupingSetDialog
-from .case_expression_dialog import CaseExpressionDialog
-from .null_functions_dialog import NullFunctionsDialog
-from .subquery_filter_dialog import SubqueryFilterDialog
-from .views_dialog import ViewsDialog
-from .materialized_views_dialog import MaterializedViewsDialog
-from .cte_dialog import CTEDialog
-
-__all__ = [
-    'TextSearchDialog',
-    'AdvancedSelectDialog',
-    'AggregateFunctionDialog',
-    'GroupingSetDialog',
-    'CaseExpressionDialog',
-    'NullFunctionsDialog',
-    'SubqueryFilterDialog',
-    'ViewsDialog',
-    'MaterializedViewsDialog',
-    'CTEDialog'
-]
+__all__ = ['WindowFunctionDialog']
